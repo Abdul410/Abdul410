0568b0320de9bb58d006b5bf762459d1437e1f74
Abdul410 <149074465+Abdul410@users.noreply.github.com>
2023-10-27 22:36:19 +0600

    Create index.js
    addition with js

---
+++ index.js
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <script src="index.js"></script>
+    <style>
+        body{
+            background-color: rgb(3, 73, 3);
+        }
+    </style>
+</head>
+<body>
+<script>
+var num1=20;
+var num2=30;
+var num3=40;
+document.write("num1="+20+",num2="+30+" and num3="+40);
+</script>
+</body> 
+</html>
