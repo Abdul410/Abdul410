2b508c3eb7ce22a355a1cb7f97fff532581689f0
Abdul410 <149074465+Abdul410@users.noreply.github.com>
2023-10-29 09:04:09 +0600

    Create concatenation.js

---
+++ concatenation.js
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <script src="index.js"></script>
+    <style>
+        body{
+            background-color: rgb(3, 73, 3);
+        }
+    </style>
+</head>
+<body>
+
+<script>
+//charAt//
+var text1 = "Bangladesh";
+var text2 = "is a beautiful country";
+var text = text1.concat(text2);
+//document.write(text1.concat(text2));
+document.write(text);
+
+</script>
+
+</body> 
+</html>
