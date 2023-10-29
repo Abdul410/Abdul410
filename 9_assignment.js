62a91da0552ac334b783dcb345855ab3e124c35c
Abdul410 <149074465+Abdul410@users.noreply.github.com>
2023-10-29 12:15:45 +0600

    Create 9_assignment.js

---
+++ 9_assignment.js
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <script src="index.js"></script>
+    <style>
+        body{
+            background-color: rgb(3, 73, 3);
+        }
+    </style>
+</head>
+<body>
+
+<script>
+    /*
+var x = 9;
+ x += 8;
+document.write(x);
+*/
+
+//////
+
+var x = 12;
+x *= 14;
+document.write(x); 
+
+</script>
+
+</body> 
+</html>
