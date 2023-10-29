f3436481089b8784eeffdf0137a794cde76ed860
Abdul410 <149074465+Abdul410@users.noreply.github.com>
2023-10-29 08:42:48 +0600

    Create charAt.js

---
+++ charAt.js
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <script src="index.js"></script>
+    <style>
+        body{
+            background-color: rgb(3, 73, 3);
+        }
+    </style>
+</head>
+<body>
+
+<script>
+//charAt//
+var text = "in a winter morning";
+text.charAt(5);
+document.write(text.charAt(12));
+
+</script>
+
+</body> 
+</html>
