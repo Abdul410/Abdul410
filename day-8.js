000ae47f45accd2d5188ec21231aeb05bc27a48b
Abdul410 <149074465+Abdul410@users.noreply.github.com>
2023-10-29 08:28:14 +0600

    Create day-8.js

---
+++ day-8.js
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <script src="index.js"></script>
+    <style>
+        body{
+            background-color: rgb(3, 73, 3);
+        }
+    </style>
+</head>
+<body>
+
+<script>
+
+var text = prompt("enter your name :");
+
+document.write("your character length is" + text.length +" meter");
+
+</script>
+
+</body> 
+</html>
