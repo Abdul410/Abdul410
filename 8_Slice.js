a2efa26c06e6f57470931fa685097a26d91557f2
Abdul410 <149074465+Abdul410@users.noreply.github.com>
2023-10-29 09:14:31 +0600

    Create 8_Slice.js

---
+++ 8_Slice.js
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <script src="index.js"></script>
+    <style>
+        body{
+            background-color: rgb(3, 73, 3);
+        }
+    </style>
+</head>
+<body>
+
+<script>
+//charAt//
+var text1 = "Bangladesh";
+var text = text1.slice(2,5);
+//document.write(text1.slice(2,5));
+document.write(text);
+
+</script>
+
+</body> 
+</html>
