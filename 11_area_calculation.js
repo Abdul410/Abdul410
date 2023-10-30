5962703a2457c3ff3f1b56456d7f57e9b56cbda6
Abdulahad <149074465+Abdul410@users.noreply.github.com>
2023-10-30 19:22:45 +0600

    Create 11_area_calculation.js

---
+++ 11_area_calculation.js
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Document</title>
+    <script src="index.js"></script>
+    <style>
+        body{
+            background-color: rgb(3, 73, 3);
+        }
+    </style>
+</head>
+<body>
+
+        <script>
+
+var height=580;
+var land=40;
+var area=.5*land*height;
+document.write("the are of the triangle is "+ .5*land*height+ " units");
+        </script>
+    </body> 
+</html>
