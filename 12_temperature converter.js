bcd51806369fcafe404f24d5e6e13650ab022ebe
Abdulahad <149074465+Abdul410@users.noreply.github.com>
2023-10-31 08:24:39 +0600

    Update 12_temperature converter.js

--- 12_temperature converter.js
+++ 12_temperature converter.js
@@ -25,6 +25,25 @@
             var C=26;
             F=(9*C)/5+32;
           document.write("Fahrenheit is"+F+" degree when celcius is 26 C");
+</script>
+
+As a variant////////
+
+         <script>
+               // Fahrenheit to Celcius//
+
+
+          var F=parseInt(prompt("Enter Fahrenheit :"));
+           C=(F-32)*(5/9);
+            document.write("Celcius is "+C+"<br>");
+
+
+                //Celcius to Fahrenheit//
+
+
+        var C=parseInt(prompt("Enter Celcius :"));
+           F=(9*C)/5+32;
+           document.write("Fahrenheit is "+F);
 
         </script>
     </body> 
