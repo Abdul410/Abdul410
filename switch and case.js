24160ec96ab330fa45e4a7c48e0c47b454f1a5f0
Abdulahad <149074465+Abdul410@users.noreply.github.com>
2023-11-12 20:44:19 +0600

    Create switch and case.js

---
+++ switch and case.js
@@ -0,0 +1,87 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>media object</title>
+    <link rel="stylesheet" href="./css/bootstrap.min.css">
+    <link rel="stylesheet" href="Media object.css">
+    <style>
+      body{
+        margin-left: 70px;
+        margin-top: 100px;
+        background-color: rgb(42, 104, 42);
+      }
+    </style>
+</head>
+<body>
+ <!--  <div class="circle1">
+        <div class="circle2">
+            <div class="circle3"></div>
+        </div>
+    </div> -->
+<script>
+num=prompt("Enter any charater :");
+switch(num){
+  case "0":
+  document.write("zero");
+  break;
+  case "1":
+  document.write("one");
+  break;
+  case "2":
+  document.write("two");
+  break;
+  case "3":
+  document.write("three");
+  break;
+  case "4":
+  document.write("four");
+  break;
+  case "5":
+  document.write("five");
+  break;
+  case "6":
+  document.write("six ");
+  break;
+  case "7":
+  document.write("seven");
+  break;
+  case "8":
+  document.write("eight");
+  break;
+  case "9":
+  document.write("nine");
+  break;
+  default:
+  document.write("there is no digit");
+
+}
+
+
+/*answer=num.toLowerCase();
+if(num==0)
+document.write("zero");
+else if(num==1)
+document.write("one");
+else if(num==2)
+document.write("two");
+else if(num==3)
+document.write("three");
+else if(num==4)
+document.write("four");
+else if(num==5)
+document.write("five")
+else if(num==6)
+document.write("six");
+else if(num==7)
+document.write("seven");
+else if(num==4)
+document.write("eight");
+else if(num==9)
+document.write('nine')
+else
+document.write("digit is not valid")*/
+</script>
+</body>
+</html>
